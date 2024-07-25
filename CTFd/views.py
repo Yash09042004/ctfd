7344551ef47e35c478e6623f4242aa2c212efce8
Yash09042004 <yashkiran2004@gmail.com>
2024-07-25 16:47:56 +0530

    working

--- CTFd/views.py
+++ CTFd/views.py
@@ -214,7 +214,7 @@ def setup():
     height: 100%;
 }}
 .glass-card {{
-    height: 80%;
+    height: 60%;
     background: rgba(255, 255, 255, 0.2);
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
