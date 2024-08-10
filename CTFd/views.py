f350e11f4fd8178e29c465be5164e9360af33f20
aunchagaonkar <ameya21unchagaonkar@gmail.com>
2024-08-10 23:43:48 +0530

    removed glass effect

--- CTFd/views.py
+++ CTFd/views.py
@@ -226,20 +226,6 @@ def setup():
 }}
 </style>    
 <div class="row">
-  <div class="col-md-6 glass-card">
-    <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="{default_ctf_banner_location}" />
-    <h3 class="text-center">
-      <p>A cool CTF platform from <a href="https://www.wcewlug.org">wlug</a></p>
-      <p>WLUG:</p>
-      <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-      <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-      <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
-    </h3>
-    <br>
-    <h4 class="text-center">
-      <a href="admin">Click here</a> to login and setup your CTF
-    </h4>
-  </div>
 </div>"""
 
             page.content = index
